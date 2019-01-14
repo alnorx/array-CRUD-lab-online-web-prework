acdaa9c2936168bc5ac0acf7130b975835184dcc
Alan N <anan112114@gmail.com>
2019-01-14 15:17:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,15 +18,18 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  array=[]
  puts array.pop
 end
 
 def remove_element_from_start_of_array(array)
+  array=[]
   puts array.shift 
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  array=[]
   puts array["#{index_number}"]
 end
 
