f339e93741e04beabdcdcc856715a03f3ffb66bc
Alan N <anan112114@gmail.com>
2019-01-14 15:09:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  s=[1,2,3,4]
+  array=[1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -19,12 +19,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array=[]
- k= array.pop
+ array=array.pop
 end
 
 def remove_element_from_start_of_array(array)
   array=[]
-  l=array.shift 
+  array=array.shift 
   
 end
 
